1477388e408439de94518da547f7ac1a21128c80
Nikolai Vinogradov <vinograd@e1z2r7p24.42.us.org>
2019-07-05 20:06:49 -0700

    July 5

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 15:13:25 by vinograd          #+#    #+#             */
-/*   Updated: 2019/07/04 13:45:20 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/07/05 20:00:44 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,52 @@ void	drow(t_dot a, t_dot b, void *mlx_ptr, void *win_ptr)
 {
 	float step_x;
 	float step_y;
+	float max;
 
 	step_x = b.x - a.x;
 	step_y = b.y - a.y;
-	while (a.x != b.x)
+	max = MAX(step_x, step_y);
+	step_x /= max;
+	step_y /= max;
+	while ((int)a.x != (int)b.x)
 	{
-		mlx_pixel_put(mlx_ptr, win_ptr, step_x, step_y, 0xBBFAFF);
-		step_x += 0.5;
-		step_y += 1;
+		mlx_pixel_put(mlx_ptr, win_ptr, a.x, a.y, 0xBBFAFF);
+		a.x += step_x;
+		a.y += step_y;
 	}
 }
 
 int		main(int argc, char **argv)
 {
-	void *mlx_ptr;
-	void *win_ptr;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_dot	i;
+	t_dot	j;
+	t_dot	i1;
+	t_dot	j1;
 
-	if (argc != 2)
-		ft_errors(1);
+	i.x = 100;
+	i.y = 100;
+	j.x = 50;
+	j.y = 200;
+	i1.x = 50;
+	i1.y = 100;
+	j1.x = 100;
+	j1.y = 200;
+	// if (argc != 2)
+	// 	ft_errors(1);
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 2000, 1000, "FDF");
-	float i = 100;
-	float j = 120;
-	while (i < 250)
-	{
-		mlx_pixel_put(mlx_ptr, win_ptr, i, j, 0xBBFAFF);
-		i += 0.125;
-		j += 1;
-	}
-	mlx_key_hook(win_ptr, deal_key, NULL);//keyboard handler
-	mlx_loop(mlx_ptr); //to keep program open
+	i.x = 100;
+	i.y = 50;
+	j.x = 50;
+	j.y = 200;
+	i1.x = 50;
+	i1.y = 100;
+	j1.x = 100;
+	j1.y = 200;
+	drow(i, j, mlx_ptr, win_ptr);
+	drow(i1, j1, mlx_ptr, win_ptr);
+	mlx_key_hook(win_ptr, deal_key, NULL);
+	mlx_loop(mlx_ptr);
 }
--- fdf.h
+++ fdf.h
@@ -6,21 +6,22 @@
 /*   By: vinograd <vinograd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 15:13:39 by vinograd          #+#    #+#             */
-/*   Updated: 2019/07/04 12:31:54 by vinograd         ###   ########.fr       */
+/*   Updated: 2019/07/05 19:24:44 by vinograd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
+# define MAX(x, y) (x > y) ? x : y;
 # include "libft/libft.h"
 # include "minilibx_macos/mlx.h"
 
 typedef struct	s_dot
 {
-	int				x;
-	int				y;
-	int				z;
+	float			x;
+	float			y;
+	float			z;
 	struct s_dot	*next;
 }				t_dot;
 
